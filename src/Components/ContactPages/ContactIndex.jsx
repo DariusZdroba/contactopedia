6103df2e6b6ca1da1125316597432132dc3fc99b
Darius <dariuszdroba@gmail.com>
2024-02-13 21:07:49 +0200

    General contact and favorite contacts

--- src/Components/ContactPages/ContactIndex.jsx
+++ src/Components/ContactPages/ContactIndex.jsx
@@ -8,6 +8,39 @@ import Footer from "../Layout/Footer";
 import Header from "../Layout/Header";
 
 export default class ContactIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contactList: [
+        {
+          id: 1,
+          name: "Zdroba Darius",
+          phone: "+40-786-152-726",
+          email: "dariuszdroba@gmail.com",
+          isFavorite: false,
+        },
+        {
+          id: 2,
+          name: "Mark Wiens",
+          phone: "+40-733-412-896",
+          email: "markwiens@gmail.com",
+          isFavorite: true,
+        },
+        {
+          id: 3,
+          name: "Jack Spicer",
+          phone: "+40-722-234-777",
+          email: "jackspicer@hotmail.com",
+          isFavorite: false,
+        },
+      ],
+    };
+  }
+
+  handleAddContact = () => {
+    alert("hello");
+  };
+
   render() {
     return (
       <div>
@@ -21,13 +54,27 @@ export default class ContactIndex extends Component {
               <RemoveAllContact />
             </div>
             <div className="row py-2">
-              <AddContact />
+              <div className="col-8 offset-2 row">
+                <AddContact handleAddContact={this.handleAddContact} />
+              </div>
             </div>
             <div className="row py-2">
-              <FavoriteContacts />
+              <div className="col-8 offset-2 row">
+                <FavoriteContacts
+                  contacts={this.state.contactList.filter(
+                    (u) => u.isFavorite === true
+                  )}
+                />
+              </div>
             </div>
             <div className="row py-2">
-              <GeneralContacts />
+              <div className="col-8 offset-2 row">
+                <GeneralContacts
+                  contacts={this.state.contactList.filter(
+                    (u) => u.isFavorite === false
+                  )}
+                />
+              </div>
             </div>
           </div>
         </div>
--- src/Components/ContactPages/GeneralContacts.jsx
+++ src/Components/ContactPages/GeneralContacts.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-
-const GeneralContacts = () => {
+import Contact from "./Contact";
+const GeneralContacts = (props) => {
   return (
-    <div>
-      <button className="btn btn-secondary form-control">
-        General Contact
-      </button>
+    <div
+      className="col-12 py-2"
+      style={{ borderRadius: "10px", backgroundColor: "#323637" }}
+    >
+      <div className="text-center text-white-50">Other Contacts</div>
+      <div className="p-2">
+        {props.contacts.map((contact, index) => (
+          <Contact contact={contact} key={index}></Contact>
+        ))}
+      </div>
     </div>
   );
 };
